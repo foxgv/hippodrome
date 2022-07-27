57781644f091db2fc0e7a0ddaab5b483e220d46a
Foxgv <Foxgv@google.com>
2022-07-27 18:28:23 +0300

    Lisyanoy G.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
         Hippodrome hippodrome = new Hippodrome(horses);
         logger.info("Начало скачек. Количество участников: {}", horses.size());
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
             TimeUnit.MILLISECONDS.sleep(200);
